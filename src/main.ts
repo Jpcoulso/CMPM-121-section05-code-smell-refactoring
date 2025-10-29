90f16b3928e1f33f6fa6b06d7b889db63a746149
Joseph Coulson <jpcoulso@ucsc.edu>
2025-10-29 14:11:08 -0700

    removed unnecessary comments

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@
 let c = 0;
 
 // These constants are for button IDs and heading text
-const a: string = "increment", b: string = "counter", h: string = "CMPM 121 Project";
+const a: string = "increment",
+  b: string = "counter",
+  h: string = "CMPM 121 Project";
 
 function setup() {
   // Create the HTML for the counter
@@ -30,54 +32,17 @@ function setup() {
 
   const buttonList: HTMLElement[] = [bI, bD, bR];
 
-  for(let button of buttonList){
+  for (const button of buttonList) {
     addListener(button, ctr);
   }
-/*
-  // Add click event to the increment button
-  bI.addEventListener("click", () => {
-    // Increase the counter by 1
-    c++;
-    // Update the counter display
-    ctr.innerHTML = `${c}`;
-    // Update the document title
-    document.title = "Clicked " + c;
-    // Change the background color based on even/odd count
-    document.body.style.backgroundColor = c % 2 ? "pink" : "lightblue";
-  });
-
-  // Add click event to the decrement button
-  bD.addEventListener("click", () => {
-    // Decrease the counter by 1
-    c--;
-    // Update the counter display
-    ctr.innerHTML = `${c}`;
-    // Update the document title
-    document.title = "Clicked " + c;
-    // Change the background color based on even/odd count
-    document.body.style.backgroundColor = c % 2 ? "pink" : "lightblue";
-  });
-
-  // Add click event to the reset button
-  bR.addEventListener("click", () => {
-    // Reset the counter to 0
-    c = 0;
-    // Update the counter display
-    ctr.innerHTML = `${c}`;
-    // Update the document title
-    document.title = "Clicked " + c;
-    // Change the background color based on even/odd count
-    document.body.style.backgroundColor = c % 2 ? "pink" : "lightblue";
-  });
 }
-*/
 function addListener(button: HTMLElement, ctr: HTMLElement) {
   button.addEventListener("click", () => {
-    if(button === document.getElementById(a)){
+    if (button === document.getElementById(a)) {
       c++;
-    }else if(button === document.getElementById("dec")){
+    } else if (button === document.getElementById("dec")) {
       c--;
-    }else if(button === document.getElementById("reset")){
+    } else if (button === document.getElementById("reset")) {
       c = 0;
     }
     ctr.innerHTML = `${c}`;
